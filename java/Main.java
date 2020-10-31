ac32994b2004b887aa74fb7db5ceb9427cc97d2a
Birb <erik_blair@outlook.com>
2020-11-01 00:16:34 +0100

    Nvm

--- java/Main.java
+++ java/Main.java
@@ -20,9 +20,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        char[] array = new char[50000];
-        for(int i=0;i<50000;i++) {
-            array[i] = 80;
-        }
+        print_string("Hello world!");
     }
 }
